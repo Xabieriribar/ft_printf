297af71b6f7e7bd448cb3999e138511fbd4dfde9
Xabieriribar <xabieriribarrevuelta@gmail.com>
2025-08-31 10:54:14 +0200

    Cambios en el parser.c

---
+++ ft_printf.c
@@ -0,0 +1,15 @@
+#include "ft_printf.h"
+
+/*
+int ft_printf(const char *format, ...)
+{
+
+}*/
+
+int main(void)
+{
+    printf("Char: %c\n", 'h');
+    printf("Char: %c\n", 0);
+    printf("Char: %c\n", 65);
+    printf("Char: %c\n", ' ');
+}
