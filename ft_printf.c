40f990ddf20f2cc8640b3d78225f350890808cc0
Xabieriribar <xabieriribarrevuelta@gmail.com>
2025-08-31 13:30:28 +0200

    Basic implementation.

--- ft_printf.c
+++ ft_printf.c
@@ -1,15 +1,47 @@
 #include "ft_printf.h"
 
 /*
+int print_char(va_list args)
+{
+    char    ch;
+
+    ch = (char) va_arg(args, int);
+    write(1, &ch, 1);
+    return (1);
+}
+int handle_specifier(const char *format, size_t *i, va_list args)
+{
+    if (format[*i] == 'c')
+        return (print_char(args));
+    return (0);
+}
+*/
 int ft_printf(const char *format, ...)
 {
+    size_t  i;
+    va_list args;
+    int  counter;
 
-}*/
-
+    i = 0;
+    counter = 0;
+    va_start(args, format);
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%' && format[i] != '\0')
+        {
+            i++;
+            counter += handle_specifier(format, &i, args);
+        } else {
+            write(1, &format[i], 1);
+            counter += 1;
+        }
+        i++;
+    }
+    va_end(args);
+    return (counter);
+}
 int main(void)
 {
-    printf("Char: %c\n", 'h');
-    printf("Char: %c\n", 0);
-    printf("Char: %c\n", 65);
-    printf("Char: %c\n", ' ');
+    int ret;
+    ret = ft_printf("hola %s", 0);
 }
