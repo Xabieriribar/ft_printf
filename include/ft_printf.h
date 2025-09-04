d6beaf80aed1be71fffca57e6404bf9f9bc23b61
Xabieriribar <xabieriribarrevuelta@gmail.com>
2025-09-04 13:48:30 +0200

    Parser improved.

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -1,65 +1,27 @@
-#ifndef STDARG_H
-# define STDARG_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <limits.h>
 
-#define BUFFER_SIZE (1 << 12)
-#define FLAGS ("+ 0#-")
-#define NUMBERS ("0123456789")
-#define SPECIFIERS ("cspdiuxX")
-#define MALLOC_ERROR -42;
-
-typedef enum e_base
-{
-    BASE_10 = 10;
-    BASE_16 = 16; 
-} t_base;
-typedef struct s_format
-{
-    bool Plus;
-    bool leftJustify;
-    bool space;
-    bool hash;
-    bool zeroPad;
-    bool uppercase;
-    int  width_value;
-    int  precision_value;
-    char specifier;
-
-} t_format;
-
-typedef struct s_data
-{
-    va_list ap;
-    const char *s;
-    char    *buffer;
-    int chars_written;
-    int buffer_index;
-    int buffer_size; 
-    t_format format;
-} t_data;
-
-
-void    ft_memset(t_data data, int c, size_t data);
 int ft_printf(const char *format, ...);
-char	*ft_itoa(int n);
-char    *ft_utoa(unsigned int n);
-int	ft_atoi(const char *nptr, int *i);
-size_t	ft_strlen(const char *s);
-int	ft_isdigit(va_list args);
-char	*ft_utoa_base(unsigned long n, int uppercase);
-void    parse_format(const char *format, size_t *i, va_list args);
+
+int ft_print_char(char c);
+int ft_print_str(const char *str);
+int ft_print_ptr(void *p);
+int ft_print_int(int n);
+int ft_print_unsigned(unsigned int n);
+int ft_print_hex(unsigned int n, int uppercase);
 int ft_print_percent(void);
-int ft_print_pointer(va_list args);
-int ft_print_unsigned(va_list args);
-int ft_print_hex(va_list args, int uppercase);
-int print_int(va_list args);
-int print_char(va_list args);
-int print_str(va_list args);
-int handle_specifier(const char *format, size_t *i, va_list args);
+
+
+size_t	ft_strlen(const char *s);
+int     ft_print_char(char c);
+int     ft_write(const char *s, size_t len);
+int     ft_print_str(char *str);
+void    ft_putnbr_base(unsigned long n, char *base);
+
 #endif
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,39 +1,40 @@
 #include "ft_printf.h"
-void    parse_format(const char *format, size_t *i, va_list args, t_format *frmt)
-{
 
-    while(format[*i] == '+' || format[*i] == '-' || format[*i] == '0' || format[*i] == ' ')
-    {
-        if (format[*i] == '+')
-            frmt.Plus = true;
-        else if (format[*i] == '-')
-            frmt.leftJustify = true;
-        else if (format[*i] == ' ')
-            frmt.space = true;
-        else if (format[*i] == '0')
-            frmt.zeroPad = true;
-        else if (format[*i] == '#')
-            frmt.hash = true;
-        (*i)++;
-    }
-}
-int handle_specifier(const char *format, size_t *i, va_list args)
+int dispatch(const char *format, size_t *i, va_list args)
 {
-    if (format[*i] == 'c')
-        return (print_char(args));
-    else if (format[*i] == 's')
-        return (print_str(args));
-    else if (format[*i] == 'd' || format[*i] == 'i') 
-        return (print_int(args));
-    else if (format[*i] == 'u')
+    if (*format == 'c')
+        return (ft_print_char(va_arg(args, char)));
+    else if (*format == 's')
+        return (ft_print_str(va_arg(args, char *));
+    else if (*format == 'd' || format[*i] == 'i' || *format) 
+        return (ft_print_int(args));
+    else if (*format == 'u')
         return (ft_print_unsigned(args));
-    else if (format[*i] == 'x')
+    else if (*format == 'x')
         return (ft_print_hex(args, 0));
-    else if (format[*i] == 'X')
+    else if (*format == 'X')
         return (ft_print_hex(args, 1));
-    else if (format[*i] == 'p')
+    else if (*format == 'p')
         return (ft_print_pointer(args));
-    else if (format[*i] == '%')
+    else if (*format == '%')
         return (ft_print_percent());
     return (0);
+}
+
+int ft_parse(const char *format)
+{
+    int counter;
+
+    counter = 0;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            counter += dispatch(*format);
+        } else {
+            counter += pf_write(*format);
+        }
+        format++;
+    }
 }
