f95a0eccf61f8c7e39bebdc82b52385bba1bdcef
Xabieriribar <xabieriribarrevuelta@gmail.com>
2025-09-05 09:56:51 +0200

    utils done.

--- src/conv/print_char.c
+++ src/conv/print_char.c
@@ -1,19 +1,6 @@
 #include "ft_printf.h"
 
-int print_char(va_list args)
-{
-    char    ch;
-    t_format    format;
-
-    ch = (char) va_arg(args, int);
-    if (format.width)
-    {
-        while (format.width > 0)
-        {
-            write(1, ' ', 1);
-            format.width--;
-        }
-    }
-    write(1, &ch, 1);
-    return (1);
+int ft_print_char(char c)
+{ 
+    return (ft_putchar(c));
 }
